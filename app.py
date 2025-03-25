a98a3df30ed76757bc1ddc20205583d2881bb4cd
Ethal07 <141444629+Ethal07@users.noreply.github.com>
2025-03-25 23:01:02 +0000

    Created login and registration framework

--- app.py
+++ app.py
@@ -1,30 +1,80 @@
-from flask import Flask
-from flask_login import LoginManager
+import sqlite3
+from flask import *
+from flask_login import *
+from werkzeug.security import generate_password_hash, check_password_hash
+#Establish connection to database
+con = sqlite3.connect("database.db")
 
+cur = con.cursor()
+
+#Fetch list of all tables
+listOfTables = cur.execute("""SELECT * FROM sqlite_master WHERE type='table';""").fetchall()
+
+#Create tables
+if 'users' in listOfTables: #Create table only if it does not exist
+    cur.execute("""
+    CREATE TABLE users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, username varchar UNIQUE NOT NULL, password_hash varchar NOT NULL);
+    """)
+
+if 'readings' in listOfTables:
+    cur.execute("""
+    CREATE TABLE readings(reading_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id int, timestamp varchar NOT NULL, reading varchar NOT NULL, FOREIGN KEY (user_id) REFERENCES users(user_id));
+    """)
+
+#commit changes
+con.commit()
+
+#terminate connection
+con.close()
+
+#Create instance of Flask class
 app = Flask(__name__)
-app.secret_key = "0010f95c9bc0cc186611f2058dbae586976723d5e847e668bf1b23141fcb3029"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-login_manager = LoginManager()
-
-login_manager.init_app(app)
-
-
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
-
-
-@app.route("/register")
-def register():
-    return "<h1>Registration Page</h1>"
-
-
-@app.route("/home")
-def home():
-    return "<h1>Homepage</h1>"
-
-
-@app.route("/login")
+@app.route("/login", methods=['POST','GET'])
 def login():
-    return "<h1>Login Page</h1>"
+    error = None
+    
+    givenUsername = request.form['registrationUsernameInput1']
+    givenPassword = request.form['registrationPasswordInput1']
+
+    if givenUsername in cur.execute("""SELECT username FROM users""").fetchall() and generate_password_hash(givenPassword, method="pbkdf2:sha256") == cur.execute("""SELECT password_hash FROM users WHERE username = (?);""", givenUsername):
+        return render_template("dashboard.html")
+
+@app.route("/register", methods=['POST','GET'])
+def register():
+
+    con = sqlite3.connect("database.db")
+
+    cur = con.cursor()
+
+    usernames = cur.execute("""SELECT username FROM users""").fetchall()
+
+
+    if request.method == 'POST':
+        username = request.form['registrationUsernameInput1']
+        password = request.form['registrationPasswordInput1']
+
+
+        if username in usernames:
+            return render_template("registration.html", error="Username already taken!")
+
+
+        hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
+
+
+        cur.execute("""
+        INSERT INTO users(username, password_hash) VALUES (?, ?);
+        """, (username, password))
+
+
+        con.commit()
+        con.close()
+
+        return redirect(url_for("login"))
+    return render_template("registration.html")
+
